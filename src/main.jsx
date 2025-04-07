1575a84595b41102d534294b153700c7385a43c9
Matheus Bailod <matheusserrute@gmail.com>
2025-04-07 16:41:25 -0300

    Add files via upload

--- src/main.jsx
+++ src/main.jsx
@@ -11,7 +11,7 @@ import App from './App.jsx'
 import Contact from './Contact.jsx'
 
 import { Analytics } from "@vercel/analytics/react"
-import { SpeedInsights } from "@vercel/speed-insights/next"
+import { SpeedInsights } from "@vercel/speed-insights/react"
 
 const router = createBrowserRouter([
   {
