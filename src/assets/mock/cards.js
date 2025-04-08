e2851ff9d728fbf9b6a502a1c3dcbe6f8a86008b
Matheus Bailod <matheusserrute@gmail.com>
2025-04-08 16:54:40 -0300

    Add files via upload

---
+++ src/assets/mock/cards.js
@@ -0,0 +1,23 @@
+import photo1 from '../images/img01.avif'
+
+export const cards = [
+    {
+        text: "primeiro", img: photo1
+    },
+    {
+        text: "segundo", img: photo1
+    },
+    {
+        text: "terceiro", img: photo1
+    },
+    {
+        text: "quarto", img: photo1
+    },
+    {
+        text: "quinto", img: photo1
+    },
+    {
+        text: "sexto", img: photo1
+    },
+
+];
